7624ca55ad16349867835c82573b717b56f35f8a
ana salas <anaprograma84@gmail.com>
2023-08-30 21:28:02 -0300

    view del cartwidget

--- src/components/Navbar/CartWidget/index.jsx
+++ src/components/Navbar/CartWidget/index.jsx
@@ -7,6 +7,14 @@ function CartWidget() {
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const [number, setNumber] = useState(0);
+  const sum = () => {
+    setNumber(number + 1);
+  };
+  const minus = () => {
+    number > 0 ? setNumber(number - 1) : null;
+  };
+  const subtotal = 4 * number;
 
   return (
     <>
@@ -22,24 +30,50 @@ function CartWidget() {
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Carrito</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <div className="container text-left">
+            <div className="row">
+              <div className="col-3">Column</div>
+              <div className="col-9">
+                <div className="row">
+                  <div className="col-10">
+                    <div>nombre producto </div>
+                    <button onClick={sum} className="buttons btn btn-link">
+                      <i className="bi bi-plus"></i>
+                    </button>
+                    <p className="Sum">{number}</p>
+                    <button onClick={minus} className="buttons btn btn-link">
+                      <i className="bi bi-dash"></i>
+                    </button>
+                    <div>Precio: 12344 </div>
+                  </div>
+                  <div className="col-2">
+                    {" "}
+                    <button className="button btn btn-link">
+                      <i className="bi bi-trash"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="total">
+            <div className="row">
+              <div className="col">
+                <h6 className="card-text">Subtotal: 123444</h6>
+              </div>
+            </div>
+          </div>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
+            Finalizar compra
           </Button>
         </Modal.Footer>
       </Modal>
     </>
-    // <div className="cart-container">
-    //   <i className="bi bi-cart3"></i>
-
-    //   <span className="number">4</span>
-    // </div>
   );
 }
 
